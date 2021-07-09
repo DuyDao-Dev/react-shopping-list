33487d923f0d800a71f3a96b66bcf7a9844133e3
Meghan Cleland <mplscleland@gmail.com>
2021-07-09 13:43:03 -0500

    set up delete request

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -3,6 +3,22 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 // TODO - Add routes here...
-
+router.delete('/list/all', (req, res) => {
+   console.log('')
+    const itemsId = req.params.id;
+    const queryText = `
+    DELETE FROM items`
+   
+   pool.query(queryText, [itemsId]) 
+}).then((response) => {
+    console.log(`What is itemsID doing?, ${itemsId}`)
+    res.sendStatus(200);
+})
+.catch(error => {
+    console.log(`Did we delete all rows? ${response.rowCount} `)
+    res.sendStatus(500);
+});
 
 module.exports = router;
+
+
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,13 @@ import React from 'react';
 import Header from '../Header/Header.jsx'
 import './App.css';
 
+const clearItemsFromDb = (shoppingList) => {
+    console.log('clear list', shoppingList);
+    axios.delete(`/list/${shoppingList}`)
+        .then(response => {
+            setShoppingList('');
+        })
+}
 
 function App() {
     return (
